fcd785868cd64f01bc0582e702611950ac66d573
Clover <yzcclover@163.com>
2019-04-13 10:47:50 +0800

    1

--- test/src/main/java/com/you/test/controller/TestController.java
+++ test/src/main/java/com/you/test/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @RequestMapping("/a")
     public TestBean A(){
         TestBean testBean = new TestBean();
-        testBean.setName("朱余朋11");
+        testBean.setName("诸葛豪");
         testBean.setAge(18);
         return testBean;
     }
